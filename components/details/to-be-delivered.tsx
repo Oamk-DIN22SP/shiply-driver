21e61e9d616a1bd1e9de3edd875f7b5f9545936e
shovoncse <shovondas26@gmail.com>
2023-11-30 18:09:16 +0200

    :sparkles: temp images are replaced with html and css

--- components/details/to-be-delivered.tsx
+++ components/details/to-be-delivered.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Button from "@/components/ui/button";
 import useCabinet from "@/hooks/use-cabinet";
+import { Dot } from "lucide-react";
 
 const ToBeDelivered = () => {
   const cabinetStore = useCabinet();
@@ -19,12 +20,29 @@ const ToBeDelivered = () => {
   };
   return (
     <>
-      <div className="aspect-square relative">
+      <div className="text-[#4A4A4A] text-[16px]">
+        <h1 className="text-[23px] font-bold leading-8">To be delivered!</h1>
+        <h2 className="font-normal leading-6">
+          This parcel should be delivered to{" "}
+          <span className="font-bold">LOCKER CENTER 5</span>.
+        </h2>
+        <p className="py-4">Follow the instructions to complete: </p>
+        <ul>
+          <li className="py-1 font-semibold">- Pick the parcel from the cabinet and close the door </li>
+          <li className="py-1 font-semibold">- Use the lock button to change the status</li>
+          <li className="py-1 font-semibold">- Check the cabinet color from the system</li>
+          <li className="py-1 font-semibold">- Deliver the parcel to related Locker center</li>
+        </ul>
+        <p className="p-8 pb-4 text-center">
+          Once cabinet is locked cabinet color in system will change from red to
+          green as below;
+        </p>
         <Image
           src="/temp/to_be_delivered.png"
           alt="Empty Cabinet"
-          fill
-          className="aspect-square rounded-md"
+          width={214}
+          height={40}
+          className="mx-auto"
         />
       </div>
       <div className="flex flex-col items-center justify-center gap-1">
