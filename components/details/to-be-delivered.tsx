c841c666b360d9eedb10e7c4cc3aaa0ceb357006
shovoncse <shovondas26@gmail.com>
2023-11-30 12:37:41 +0200

    :sparkles: delivery pickedup and change status

--- components/details/to-be-delivered.tsx
+++ components/details/to-be-delivered.tsx
@@ -2,8 +2,21 @@
 
 import Image from "next/image";
 import Button from "@/components/ui/button";
+import useCabinet from "@/hooks/use-cabinet";
 
 const ToBeDelivered = () => {
+  const cabinetStore = useCabinet();
+
+  const onPickUp = () => {
+    // api call to update status
+    cabinetStore.setState({ state: "picked-up" });
+    // find id in store data and update status
+    cabinetStore.data.find((cabinet) => {
+      if (cabinet.id === cabinetStore.activeCabinetId) {
+        cabinet.status = "empty-locker";
+      }
+    });
+  };
   return (
     <>
       <div className="aspect-square relative">
@@ -15,7 +28,7 @@ const ToBeDelivered = () => {
         />
       </div>
       <div className="flex flex-col items-center justify-center gap-1">
-        <Button onClick={() => {}} disabled={false} className="w-fit mt-6 bg-[#872222]">
+        <Button onClick={onPickUp} className="w-fit mt-6 bg-[#872222]">
           Lock
         </Button>
         <small>Finalise the delivery!.</small>
